6e52cab69c6862e8f4abd76c5e68d761a32a5095
Daiki0120 <dusui2017@kellogg.northwestern.edu>
2016-10-14 01:14:23 -0500

    s

--- app.js
+++ app.js
@@ -6,15 +6,9 @@ $(document).ready(function() {
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'℃'+'</h2>';
 
-      html += '<ul><li>'+weather.city+'</li>';
 
-      html += '<li class="currently">'+weather.currently+'</li>';
-
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-
-
-            for(var i=0;i<weather.forecast.length;i++) {
-              html += '<p>'+weather.forecast[i].day+': '+weather.forecast[i].high+'</p>';
+            for(var i=7;i<weather.forecast.length;i++) {
+              html += '<h3>'+weather.forecast[i].day+': '+weather.forecast[i].high+'</h3>';
             }
 
       $("#weather").html(html);
